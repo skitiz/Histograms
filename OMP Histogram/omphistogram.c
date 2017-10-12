f2e3d32902b305a85075c6f8df36898ad2dba10c
Kshitij Bantupalli <kshitij.bantupalli@hotmail.com>
2017-10-12 02:30:51 -0400

    Fixed Segfault #261354
    Always add debug info.

--- OMP Histogram/omphistogram.c
+++ OMP Histogram/omphistogram.c
@@ -11,88 +11,87 @@ float* determine_intervals(int min, int max, long int intervals);
 
 int main(int argc, char* argv[])
 {
-	long int intervals;
-	int max, min;
-	unsigned long long int size;
-	int* buffer = NULL;
-	float* endpoints = NULL;
+		long int intervals;
+		int max, min;
+		unsigned long long int size;
+		int* buffer = NULL;
+		float* endpoints = NULL;
 
-	int thread_count = strtol(argv[1], NULL, 10);
+		int thread_count = strtol(argv[1], NULL, 10);
 
-	//File name
-	char* s = argv[2];
+		//File name
+		char* s = argv[2];
 
-	//Get the number of intervals
-	intervals = get_intervals(argv[3]);
-	printf("%ld", intervals);
+		//Get the number of intervals
+		intervals = get_intervals(argv[3]);
+		printf("%ld", intervals);
 
-	//Determine max-min
-	buffer = max_min(s, &size, &max, &min);
+		//Determine max-min
+		buffer = max_min(s, &size, &max, &min);
 
-	//Determine intervals
-	endpoints = determine_intervals(min, max, intervals);
+		//Determine intervals
+		endpoints = determine_intervals(min, max, intervals);
 
-	for(long int i=0; i< intervals; i++)
-	{
-		printf("%6f , ", endpoints[i]);
-	}
+		for(long int i=0; i< intervals; i++)
+		{
+			printf("%6f , ", endpoints[i]);
+		}
 
-	printf("\nEnd of program.");
-	return 0;
+		printf("\nEnd of program.");
+		return 0;
 }
 
 long int get_intervals(char* s)
 {
-	char* temp;
-	long int num = strtol(s, &temp, 10);
-	return num;
+		char* temp;
+		long int num = strtol(s, &temp, 10);
+		return num;
 }
 
 int* max_min(char* filename, unsigned long long int* size, int *max, int *min)
 {
-	FILE *fp;
-	*max = INT_MIN;
-	*min = INT_MAX;
-	struct stat file_stat;
-	unsigned long long int amount;
-	int* buffer = NULL;
+		FILE *fp;
+		*max = INT_MIN;
+		*min = INT_MAX;
+		struct stat file_stat;
+		unsigned long long int amount;
+		int* buffer = NULL;
 
-	fp = fopen(filename, "r");
-	if(fp == NULL)
-	{
-		printf("\nFile doesn't exist.")
-	}
-	int result = stat(filename, &file_stat);
-	if(result == -1)
-	{
-		printf("\nFile invalid.")
-	}
-	*size = file_stat.st_size;
-	*size /= sizeof(int);
-	buffer = malloc(*size *sizeof(int));
-	if(buffer)
-	{
-		amount = fread(buffer, sizeof(int), *size, fp);
-		if(amount == NULL)
+		fp = fopen(filename, "r");
+		if(fp == NULL)
 		{
-			printf("\nCouldn't read.")
+			printf("\nFile doesn't exist.")
 		}
-	}
-	else
-		printf("\nValue of malloc didn't succed.");
-	for(unsigned long long int i = 0; i < *size; i++)
-	{
-		if((buffer[i]) < *min)
+		int result = stat(filename, &file_stat);
+		if(result == -1)
 		{
-			*min = buffer[i];
+			printf("\nFile invalid.")
 		}
-		if((buffer[i]) > *max)
+		*size = file_stat.st_size;
+		*size /= sizeof(int);
+		buffer = malloc(*size *sizeof(int));
+		if(buffer)
 		{
-			*max = buffer[i];
+			amount = fread(buffer, sizeof(int), *size, fp);
+			if(amount == NULL)
+			{
+				printf("\nCouldn't read.")
+			}
 		}
-
-	}
-	return buffer;
+		else
+			printf("\nValue of malloc didn't succed.");
+			for(unsigned long long int i = 0; i < *size; i++)
+			{
+				if((buffer[i]) < *min)
+				{
+					*min = buffer[i];
+				}
+				if((buffer[i]) > *max)
+				{
+					*max = buffer[i];
+				}
+			}
+		return buffer;
 }
 
 float* determine_intervals(int min, int max, long int intervals)
