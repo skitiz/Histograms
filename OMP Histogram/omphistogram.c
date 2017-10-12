5e1fce932fe43e4b518255624240c08423448825
Kshitij Bantupalli <kshitij.bantupalli@hotmail.com>
2017-10-12 03:55:49 -0400

    Final edits
    Have to test if it still runs

--- OMP Histogram/omphistogram.c
+++ OMP Histogram/omphistogram.c
@@ -5,15 +5,25 @@
 #include <sys/stat.h>
 #include <limits.h>
 #include <assert.h>
+#include <time.h>
 
 long int get_intervals(char* s);
 int* max_min(char* filename, unsigned long long int* size, int* min, int* max);
 float* determine_intervals(int min, int max, long int intervals);
 unsigned long long int* occurences(int* buffer, unsigned long long int size, long int intervals, float* endpoints);
 size_t determine_index(int temp, float* endpoints, long int intervals);
+void display_histogram(long int intervals, unsigned long long int* occurences, float* endpoints)
 
 int main(int argc, char* argv[])
 {
+		clock_t begin = clock();
+
+		if(argc < 3)
+		{
+			printf("Too less arguments.");
+			exit(0);
+		}
+
 		long int intervals;
 		int max, min;
 		unsigned long long int size;
@@ -21,6 +31,7 @@ int main(int argc, char* argv[])
 		float* endpoints = NULL;
 		unsigned long long int* occurences = NULL;
 
+		if(argv[1] )
 		int thread_count = strtol(argv[1], NULL, 10);
 
 		//File name
@@ -42,13 +53,36 @@ int main(int argc, char* argv[])
 		endpoints = determine_intervals(min, max, intervals);
 
 		//Determine occurances of each number
-		occurences = count_occurrences(buffer, size, intervals, endpoints);
+		occurences = count_occurences(buffer, size, intervals, endpoints);
+
+		//Display histogram
+		display_histogram(intervals, occurences, endpoints);
 
 		printf("\nEnd of program.");
+
+		free(buffer);
+		free(occurences);
+		free(endpoints);
+		clock_t end = clock();
+		double time_spent = (double)(end-begin) / CLOCKS_PER_SEC;
+		printf("\nTime spent = %f", time_spent);
 		return 0;
 }
 
-unsigned long long int* count_occurrences(int* buffer, unsigned long long int size, long int intervals, float* endpoints)
+void display_histogram (long int intervals, unsigned long long int* occurences, float* endpoints)
+{
+	assert (occurences != NULL);
+	assert (endpoints != NULL);
+
+	float length = endpoints[1] - endpoints [0];
+	for (size_t i = 0; i < intervals ; ++i)
+	{
+		printf("%f - %f", endpoints[i], endpoints[i] + length);
+		printf("%lld\n", occurences[i] );
+	}
+}
+
+unsigned long long int* count_occurences(int* buffer, unsigned long long int size, long int intervals, float* endpoints)
 {
 	// Error checks
 	assert (buffer != NULL);
@@ -65,17 +99,19 @@ unsigned long long int* count_occurrences(int* buffer, unsigned long long int si
 	for(unsigned long int i = 0; i< size; i++)
 	{
 		size_t index = determine_index (buffer[i], endpoints, intervals);
-		num_occurrences[index]++;
+		occurences[index]++;
 	}
-	return num_occurrences;
+	return occurences;
 }
 
 size_t determine_index(int temp, float* endpoints, long int intervals)
 {
 	assert (endpoints != NULL);
-	size_t index = 0;
-	while (index < intervals && endpoints[index] > temp)
-		++index;
+	size_t index;
+	for( index = 0; index < intervals-1; index ++)
+	{
+		if( temp <= endpoints[index]) break;
+	}
 	return index;
 }
 
