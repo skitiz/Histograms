6c5de0b75fd3f9c300ee53f3cbc3e9c9c0a40c30
Kshitij Bantupalli <kshitij.bantupalli@hotmail.com>
2017-10-12 03:03:09 -0400

    Added data count
    fml

--- OMP Histogram/omphistogram.c
+++ OMP Histogram/omphistogram.c
@@ -4,10 +4,13 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <limits.h>
+#include <assert.h>
 
 long int get_intervals(char* s);
 int* max_min(char* filename, unsigned long long int* size, int* min, int* max);
 float* determine_intervals(int min, int max, long int intervals);
+unsigned long long int* occurences(int* buffer, unsigned long long int size, long int intervals, float* endpoints);
+size_t determine_index(int temp, float* endpoints, long int intervals);
 
 int main(int argc, char* argv[])
 {
@@ -16,15 +19,21 @@ int main(int argc, char* argv[])
 		unsigned long long int size;
 		int* buffer = NULL;
 		float* endpoints = NULL;
+		unsigned long long int* occurences = NULL;
 
 		int thread_count = strtol(argv[1], NULL, 10);
 
 		//File name
 		char* s = argv[2];
+		if(s == NULL)
+		{
+			printf("Invalid file name.");
+			exit(0);
+		}
 
 		//Get the number of intervals
 		intervals = get_intervals(argv[3]);
-		printf("%ld", intervals);
+		//printf("%ld", intervals);
 
 		//Determine max-min
 		buffer = max_min(s, &size, &max, &min);
@@ -32,13 +41,42 @@ int main(int argc, char* argv[])
 		//Determine intervals
 		endpoints = determine_intervals(min, max, intervals);
 
-		for(long int i=0; i< intervals; i++)
-		{
-			printf("%6f , ", endpoints[i]);
-		}
+		//Determine occurances of each number
+		occurences = count_occurrences(buffer, size, intervals, endpoints);
 
 		printf("\nEnd of program.");
 		return 0;
+}
+
+unsigned long long int* count_occurrences(int* buffer, unsigned long long int size, long int intervals, float* endpoints)
+{
+	// Error checks
+	assert (buffer != NULL);
+	assert (endpoints != NULL);
+	//
+
+	unsigned long long int* occurences = calloc (intervals, sizeof(unsigned long long int));
+	if( occurences == NULL)
+	{
+		printf("Memory allocation failed. Exiting...");
+		exit(0);
+	}
+	#pragma omp parallel for
+	for(unsigned long int i = 0; i< size; i++)
+	{
+		size_t index = determine_index (buffer[i], endpoints, intervals);
+		num_occurrences[index]++;
+	}
+	return num_occurrences;
+}
+
+size_t determine_index(int temp, float* endpoints, long int intervals)
+{
+	assert (endpoints != NULL);
+	size_t index = 0;
+	while (index < intervals && endpoints[index] > temp)
+		++index;
+	return index;
 }
 
 long int get_intervals(char* s)
@@ -60,12 +98,14 @@ int* max_min(char* filename, unsigned long long int* size, int *max, int *min)
 		fp = fopen(filename, "r");
 		if(fp == NULL)
 		{
-			printf("\nFile doesn't exist.")
+			printf("\nFile doesn't exist.");
+			exit(0);
 		}
 		int result = stat(filename, &file_stat);
 		if(result == -1)
 		{
-			printf("\nFile invalid.")
+			printf("\nFile invalid.");
+			exit(0);
 		}
 		*size = file_stat.st_size;
 		*size /= sizeof(int);
@@ -73,9 +113,10 @@ int* max_min(char* filename, unsigned long long int* size, int *max, int *min)
 		if(buffer)
 		{
 			amount = fread(buffer, sizeof(int), *size, fp);
-			if(amount == NULL)
+			if(amount == 0)
 			{
-				printf("\nCouldn't read.")
+				printf("\nCouldn't read.");
+				exit(0);
 			}
 		}
 		else
