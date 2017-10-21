aa377defb74e1cba0d58c59883dc27acf33d91f1
Kshitij <kshitij.bantupalli@gmail.com>
2017-10-21 17:28:21 -0400

    Changes

--- OMPMPI/omphistogram2.c
+++ OMPMPI/omphistogram2.c
@@ -14,7 +14,7 @@ unsigned long long int* count_occurences(int* buffer, unsigned long long int siz
 size_t determine_index(int temp, float* endpoints, long int intervals);
 void display_histogram(long int intervals, unsigned long long int* occurences, float* endpoints);
 void errorhandler(error_code);
-void broadcastdata(long int* intervals, int* min, int* max, unsigned long long int* size, unsigned long long int* local_size, MPI_Comm comm);
+void broadcastdata(char* s, char* filename, long int* intervals, int* max, int* min, unsigned long long int* local_size, unsigned long long int* size, int buffer[], int my_rank, MPI_Comm comm);
 
 int main(int argc, char* argv[])
 {
@@ -56,14 +56,17 @@ int main(int argc, char* argv[])
 			exit(0);
 		}
 
+
+		broadcastdata(argv[3], argv[2], &intervals, &max, &min, &local_size, &size, local_buffer, my_rank, comm, comm_sz);
 		//Get the number of intervals
-		if(my_rank == 0)
-		{
-			intervals = get_intervals(argv[3]);
-		}
+		//if(my_rank == 0)
+		//{
+		//	intervals = get_intervals(argv[3]);
+		//}
 		//printf("%ld", intervals);
 
 		//Determine max-min
+		/*
 		if(my_rank == 0)
 		{
 			buffer = max_min(s, &size, &max, &min);
@@ -71,8 +74,9 @@ int main(int argc, char* argv[])
 			local_buffer = (local_size * sizeof(int));
 
 		}
-		void broadcastdata(&intervals, &min, &max, &size, &local_size, comm);
-		errorhandler(MPI_Scatter(buffer,local_size,MPI_INT,local_buffer,local_size,MPI_INT, 0, comm));
+		*/
+		//broadcastdata(&intervals, &min, &max, &size, &local_size, comm);
+		errorhandler(MPI_Scatter(buffer, local_size, MPI_INT, local_buffer, local_size, MPI_INT, 0, comm));
 
 		//Determine intervals
 		endpoints = determine_intervals(min, max, intervals);
@@ -140,13 +144,17 @@ size_t determine_index(int temp, float* endpoints, long int intervals)
 	return index;
 }
 
+/*
+
 long int get_intervals(char* s)
 {
 		char* temp;
 		long int num = strtol(s, &temp, 10);
 		return num;
 }
+*/
 
+/*
 int* max_min(char* filename, unsigned long long int* size, int *max, int *min, unsigned long long int* amount)
 {
 		FILE *fp;
@@ -198,20 +206,23 @@ int* max_min(char* filename, unsigned long long int* size, int *max, int *min, u
 			}
 		return buffer;
 }
+*/
 
-float* determine_intervals(int min, int max, long int intervals)
-{
-	float* endpoints = malloc(intervals * sizeof(float));
-	float length = (max-min) / (float) intervals;
-	float temp = min;
-	//#pragma omp parallel for
-	for(size_t i = 0; i < intervals; i++)
+
+	float* determine_intervals(int min, int max, long int intervals)
 	{
-		endpoints[i] = temp;
-		temp += length;
+		float* endpoints = malloc(intervals * sizeof(float));
+		float length = (max-min) / (float) intervals;
+		float temp = min;
+		//#pragma omp parallel for
+		for(size_t i = 0; i < intervals; i++)
+		{
+			endpoints[i] = temp;
+			temp += length;
+		}
+		return endpoints;
 	}
-	return endpoints;
-}
+
 
 void errorhandler(int error_code)
 {
@@ -224,12 +235,73 @@ void errorhandler(int error_code)
 	}
 }
 
-void broadcastdata(long int* intervals, int* min, int* max, unsigned long long int* size, unsigned long long int* local_size, MPI_Comm comm)
+void broadcastdata(char* s, char* filename, long int* intervals, int* min, int* max, unsigned long long int* local_size, unsigned long long int* size, int local_buffer[], int my_rank, MPI_Comm comm, int comm_sz)
 {
-	errorhandler(MPI_Bcast(intervals, 1, MPI_LONG_INT, 0, comm);
+	if( my_rank == 0)
+	{
+		char* temp;
+		long int interval = strtol(s, &temp, 10);
+		*intervals = &interval;
+
+		FILE* fp;
+		*max = INT_MIN;
+		*min = INT_MAX;
+		struct stat file_stat;
+		unsigned long long int amount;
+		int* buffer = NULL;
+
+		fp = fopen(filename, "r");
+		if(fp == NULL)
+		{
+			printf("\nFile doesn't exist.");
+			exit(0);
+		}
+		int result = stat(filename, &file_stat);
+		if(result == -1)
+		{
+			printf("\nFile invalid.");
+			exit(0);
+		}
+		*size = file_stat.st_size;
+		*size /= sizeof(int);
+		*local_size = *size / comm_sz;
+		buffer = malloc( *size *sizeof(int));
+		local_buffer = malloc( *local_size * sizeof(int));
+
+		if(buffer)
+		{
+			amount = fread(buffer, sizeof(int), *size, fp);
+			if(amount == 0)
+			{
+				printf("\nCouldn't read.");
+				exit(0);
+			}
+		}
+		else
+		{
+			printf("\nValue of malloc didn't succed.");
+		}
+
+		for(unsigned long long int i = 0; i < *size; i++)
+			{
+				if((buffer[i]) < *min)
+				{
+					*min = buffer[i];
+				}
+				if((buffer[i]) > *max)
+				{
+					*max = buffer[i];
+				}
+			}
+	}
+	errorhandler(MPI_Bcast(intervals, 1, MPI_LONG_INT, 0, comm));
 	errorhandler(MPI_Bcast(min, 1, MPI_INT, 0, comm));
 	errorhandler(MPI_Bcast(max, 1, MPI_INT, 0, comm));
 	errorhandler(MPI_Bcast(size, 1, MPI_LONG_LONG_INT, 0, comm));
 	errorhandler(MPI_Bcast(local_size, 1, MPI_LONG_LONG_INT, 0, comm));
+	if(my_rank == 0)
+	{
+		free(buffer);
+	}
 }
-}
+
