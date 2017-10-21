e25b571071cd267e9018a4052f3d833c9a95e8fa
Kshitij <kshitij.bantupalli@gmail.com>
2017-10-21 19:37:24 -0400

    Alan Silvestri

--- OMPMPI/ompmpi.c
+++ OMPMPI/ompmpi.c
@@ -15,7 +15,7 @@ unsigned long long int* count_occurences(int* buffer, unsigned long long int siz
 size_t determine_index(int temp, float* endpoints, long int intervals);
 void display_histogram(long int intervals, unsigned long long int* occurences, float* endpoints);
 void errorhandler(error_code);
-void broadcastdata(char* s, char* filename, long int* intervals, int* max, int* min, unsigned long long int* local_size, unsigned long long int* size, int local_buffer[], int my_rank, MPI_Comm comm, int buffer[]);
+void broadcastdata(char* s, char* filename, long int* intervals, int* max, int* min, unsigned long long int* local_size, unsigned long long int* size, int local_buffer[], int my_rank, MPI_Comm comm);
 
 int main(int argc, char* argv[])
 {
